5e62cae3c8adf7e1a3132e0536fbb2c7c0bc1849
Belinda Hare <BelindaHare@Belindas-MacBook-Air.local>
2014-10-01 13:33:22 -0400

    added new nav button for my store, added home logo to welcome index and application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
   def login_links
     if user_signed_in?
       (link_to "Account", edit_user_registration_path) +
-      (link_to "Log Out", destroy_user_session_path, method: :delete)
+      (link_to "Log Out", destroy_user_session_path, method: :delete) +
+      (link_to "My Store", grocers_path)
     else
       (link_to "Log In", new_user_session_path) +
       (link_to "Sign Up", new_user_registration_path)
